371e18e4b4facd257182fdb79ba9d9df38df284d
Abdelrhman Ahmed <abdelrhmanhsh@gmail.com>
2022-06-29 10:31:21 +0200

    pass change

--- E-Commerce Application Admin/Utils/Constants.swift
+++ E-Commerce Application Admin/Utils/Constants.swift
@@ -12,7 +12,7 @@ struct Constants {
     // endpoints
     static let endPointCreateCustomer = "customers.json"
     static let locationId = 66823782573  //for post inventory
-    static let loginPassword = "123"
+    static let loginPassword = "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"
 }
 
 struct ErrorMessages {
